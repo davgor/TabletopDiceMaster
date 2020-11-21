dbf1da82267df947e7a5290bd1ad1f80fee487fd
davgor <davgor4@gmail.com>
2020-11-21 14:00:42 -0500

    Favorites baseline set up
    Build base for favorites screen

---
+++ DiceMaster/DiceMaster/ViewModels/FavoriteViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DiceMaster.data;
+using DiceMaster.models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceMaster.ViewModels
+{
+    public class FavoriteViewModel : BaseViewModel
+    {
+        public ObservableCollection<EntireRoll> FavoritesLog { get; } = new ObservableCollection<EntireRoll>();
+        public FavoriteViewModel()
+        {
+            updateDb();
+        }
+        public void updateDb()
+        {
+            Task<List<SQLiteEntireRoll>> SQLiteList = App.Database.GetFavorites();
+            foreach (SQLiteEntireRoll sQLiteEntireRoll in SQLiteList.Result)
+            {
+                EntireRoll entireRoll = new EntireRoll();
+                entireRoll.convertSQL(sQLiteEntireRoll);
+                FavoritesLog.Add(entireRoll);
+            }
+        }
+    }
+}
--- DiceMaster/DiceMaster/Views/FavoritePage.xaml.cs
+++ DiceMaster/DiceMaster/Views/FavoritePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiceMaster.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace DiceMaster.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FavoritePage : ContentPage
     {
+        public FavoriteViewModel _viewModel;
         public FavoritePage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = new FavoriteViewModel();
         }
     }
 }
